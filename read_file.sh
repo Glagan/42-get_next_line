a957ba7162e65353cab0fc596e80fb612c40d3dd
Nicolas COLOMER <ncolomer@e2r2p4.42.fr>
2019-11-15 13:13:25 +0100

    Fix read_file for /dev/random

--- read_file.sh
+++ read_file.sh
@@ -1,12 +1,16 @@
 if [ $# -ge 2 ]; then
 	rm -f read_file
 	gcc -Wall -Wextra -Werror -D BUFFER_SIZE=$1 -I. get_next_line.c get_next_line_utils.c read_file.c -o read_file
-	./read_file < $2 | cat -e > y
-	cat -e $2 > r
-	if [ $# -eq 3 ]; then
-		diff -y r y
+	if [ $2 = "/dev/random" ]; then
+		./read_file	< $2
 	else
-		diff -y --suppress-common-lines r y
+		./read_file < $2 | cat -e > y
+		cat -e $2 > r
+		if [ $# -eq 3 ]; then
+			diff -y r y
+		else
+			diff -y --suppress-common-lines r y
+		fi
 	fi
 	rm -f read_file y r
 else
